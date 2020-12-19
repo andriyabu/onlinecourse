c357c6af196495ecd458e391bb1803b1489f1098
Andri Yabu <andriyabu@gmail.com>
2020-12-19 12:11:23 +0700

    create service-media

--- api-gateway/routes/apiAdapter.js
+++ api-gateway/routes/apiAdapter.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const {TIME_OUT } = process.env;
 
 module.exports = (baseUrl) => {
-    return axios.crete({
+    return axios.create({
         baseUrl: baseUrl,
         timeout: TIME_OUT
     })
