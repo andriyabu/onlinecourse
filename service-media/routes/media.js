dd5b8e8fcc565a337f33f57aec2421d914b1a348
Andri Yabu <andriyabu@gmail.com>
2020-12-19 20:48:51 +0700

    create route delete to delete image

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,10 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 
 const { Media } = require('../models');
+const { route } = require('.');
+const fs = require('fs');
 
+//route get all data
+router.get('/', async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id','image']
+  });
+  
+  const mapImage = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  });
+   res.json({
+     status: 'success',
+     data: mapImage
+   });
+});
+
+//route post data
 router.post('/', (req, res) => {
   // receive image from request body
   const image = req.body.image;
@@ -25,7 +44,7 @@ router.post('/', (req, res) => {
     const filename = filepath.split('/').pop();
 
     // save image to tanble media
-    const media = await Media.create({ image: `image/${filename}`});
+    const media = await Media.create({ image: `images/${filename}`});
 
     return res.json({
       status: 'success',
@@ -36,4 +55,29 @@ router.post('/', (req, res) => {
     });
   });
 });
+
+// route delete data
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const media = await Media.findByPk(id);
+
+  if (!media) {
+    return  res.status(404).json({status: 'error', message: 'media not found!'});
+  }
+
+  fs.unlink(`./public/${media.image}`, async (err) => {
+    if (err) {
+      return res.status(400).json({status: 'error', message: err.message});
+    }
+
+    await media.destroy();
+
+    return  res.json({
+      status: 'success',
+      message: 'image deleted'
+    });
+  });
+});
+
 module.exports = router;
